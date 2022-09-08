05a4103f9e9ddb55856494796705ae839c008eb4
RaBeena21 <rabinamrz2001@gmail.com>
2022-09-08 10:53:41 +0545

    TODO

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import City from './components/City';
+import Todo from './components/Todo';
 
 function App() {
   return(
     <div className="App">
-      <City/>
-      <City city="Pokhara" features="fantastic"/>
-      <City city="ktm" features="Majestic"/>
-      <City city="Dhading" features="marvelous"/>
-      <City city="london City" features="beautiful"/>
-
-      {/* <p>Hello,i am in london City. it is beautiful</p>
-      <p>Hello,i am in pokhara. it is fantastic</p>
-      <p>Hello,i am in ktm. it is Majestic</p>
-      <p>Hello,i am in Dhading. it is marvelous</p> */}
+      <Todo/>
     </div>
 );
   }
